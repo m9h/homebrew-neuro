f5016eeafdba8b022ed05f9d7fa82da5f49eb4d2
Morgan Hough <morgan.g.hough@gmail.com>
2026-05-08 21:26:53 -0700

    Update c3d dependency path

--- Formula/c3d.rb
+++ Formula/c3d.rb
@@ -6,7 +6,7 @@ class C3d < Formula
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
-  depends_on "mhough/neuro/itk-neuro"
+  depends_on "m9h/neuro/itk-neuro"
 
   def install
     args = %w[
